0c4e8857f6dba41667c7a3d592f14f49c5d5da95
shahadat-hossen008 <penaxavier078@gmail.com>
2026-03-03 15:18:19 +0600

    Feat: create reference tag type

--- src/sanity/schemaTypes/about.ts
+++ src/sanity/schemaTypes/about.ts
@@ -1,38 +1,42 @@
+import { InfoOutlineIcon } from "@sanity/icons";
 import { defineField, defineType } from "sanity";
 
 export const about = defineType({
-    name: "about",
-    title: "About",
-    type: "document",
-    fields: [
-        defineField({
-            name: "title",
-            type: "string",
-        }),
-        defineField({
-            name: "description",
-            title: "Description",
-            type: "array",
-            of: [{ type: "block" }],
-            validation: (Rule) => Rule.required(),
-        }),
-        defineField({
-            name: 'socialLinks',
-            type: 'array',
-            of: [{
-                type: 'object',
-                fields: [
-                { name: 'platform', type: 'string' },
-                { name: 'url', type: 'url' }
-                ]
-            }]
-        }),
-        defineField({
-            name: 'technologies',
-            title: "Technologies",
-            description: "Define the technologies used in the project",
-            type: 'array',
-            of: [{ type: 'string' }]
-        })
-    ],
-})
+  name: "about",
+  title: "About",
+  type: "document",
+  icon: InfoOutlineIcon,
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "array",
+      of: [{ type: "block" }],
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "socialLinks",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            { name: "platform", type: "string" },
+            { name: "url", type: "url" },
+          ],
+        },
+      ],
+    }),
+    defineField({
+      name: "technologies",
+      title: "Technologies",
+      description: "Define the technologies used in the project",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "tag" }] }],
+    }),
+  ],
+});
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { about } from './about'
+import { type SchemaTypeDefinition } from "sanity";
+import { about } from "./about";
+import { tagType } from "./tagType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [about],
-}
+  types: [about, tagType],
+};
