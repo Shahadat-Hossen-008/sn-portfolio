5a2b00afb9be96b44e24882abf25a15cf33a7a2a
shahadat-hossen008 <penaxavier078@gmail.com>
2026-03-03 19:35:03 +0600

    Fix: fix commented issues

--- src/sanity/schemaTypes/profile.ts
+++ src/sanity/schemaTypes/profile.ts
@@ -1,52 +1,46 @@
-import {UserIcon} from '@sanity/icons'
 import { defineField, defineType } from "sanity";
 
 export const profile = defineType({
-    name: 'profile',
-    title: 'Profile',
-    type: 'document',
-    // icon: UserIcon ,
-    fields: [
-        defineField({
-            name: 'fullName',
-            title: 'Full Name',
-            type: 'string',
-            validation: (Rule) => Rule.required()
-        }),
-        defineField({
-            name: 'headline',
-            title: 'Headline',
-            type: 'string',
-            description: 'In one line, what is your role?',
-            validation: (Rule) => Rule.required().max(60)
-        }),
-        defineField({
-            name: 'bio',
-            title: 'Bio',
-            type: 'array',
-            description: 'Write a few sentences about yourself',
-            of:[{type: 'block'} ]
-        }),
-         defineField({
-            name: 'image',
-            title: 'Image',
-            description: 'Upload your profile picture',
-            type: 'image',
-            options:{ hotspot: true }
-         }),
-         defineField({
-            name: 'uploadCV',
-            title: 'Upload CV',
-            description: 'Upload your CV',
-            type: 'file',
-         }),
-        //  defineField({
-        //     name: 'contactMe',
-        //     title: 'Contact Me',
-        //     description: 'Add your contact details',
-        //     type: 'reference',
-        //     to: [{type: 'contact'}]
-        //  })
-
-    ]
-})
+  name: "profilePage",
+  title: "Profile Page",
+  type: "document",
+  fields: [
+    defineField({
+      name: "fullName",
+      title: "Full Name",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "headline",
+      title: "Headline",
+      type: "string",
+      description: "In one line, what is your role?",
+      validation: (Rule) => Rule.required().max(60),
+    }),
+    defineField({
+      name: "bio",
+      title: "Bio",
+      type: "array",
+      description: "Write a few sentences about yourself",
+      of: [{ type: "block" }],
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      description: "Upload your profile picture",
+      type: "image",
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: "uploadCV",
+      title: "Upload CV",
+      description: "Upload your CV (PDF only)",
+      type: "file",
+      options: {
+        accept: "application/pdf",
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+});
--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -1,23 +1,21 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure";
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
-import {UserIcon} from '@sanity/icons'
+import { UserIcon } from "@sanity/icons";
 
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       // Singleton Profile
       S.listItem()
-        .title('Profile')
-        .icon(UserIcon)                 
+        .title("Profile Page")
+        .icon(UserIcon)
         .child(
-          S.document()
-            .schemaType('profile')
-            .documentId('profileSingleton') 
+          S.document().schemaType("profilePage").documentId("profilePage"),
         ),
       // All other document types except 'profile'
       ...S.documentTypeListItems().filter(
-        (item) => item.getId() !== 'profile'
-      )
-    ])
+        (item) => item.getId() !== "profilePage",
+      ),
+    ]);
