d3974e0c17db56dde2f8470dd21458f7675caa42
shahadat-hossen008 <penaxavier078@gmail.com>
2026-03-06 16:49:49 +0600

    Feat: fetch profile data

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,11 +1,17 @@
 import { RadialGradientBackground } from "@/components/background/RadialGradientBackground";
+import Profile from "@/components/profile/Profile";
+import { sanityFetch } from "@/sanity/lib/live";
+import { PROFILE_QUERY } from "@/sanity/lib/queries";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const { data: profile } = await sanityFetch({ query: PROFILE_QUERY });
+  console.log(profile);
+
   return (
     <div className="h-350">
       <RadialGradientBackground variant="hero" />
-      <h1 className="3xl">Hey there</h1>
+      <Profile />
     </div>
   );
 }
