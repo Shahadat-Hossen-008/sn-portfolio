1658955549663032478f81fea3359c5eed64024a
shahadat-hossen008 <penaxavier078@gmail.com>
2026-03-18 03:00:46 +0600

    Rename folder to meaningful way

--- src/sanity/objects/customImage.ts
+++ src/sanity/objects/customImage.ts
@@ -1,6 +1,6 @@
 import { ImageIcon } from "@sanity/icons";
 import { defineField, defineType } from "sanity";
-import { customImageProps } from "../schemaTypes/types/customImage";
+import { customImageProps } from "../schemaTypes/schemaPropsTypes/customImage";
 
 export const customImage = ({
   name,
