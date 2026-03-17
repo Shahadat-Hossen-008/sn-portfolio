5ea7e9de8d7972535df2c6740e863a5c0b038b8a
shahadat-hossen008 <penaxavier078@gmail.com>
2026-03-17 16:58:29 +0600

    Add all schema in index

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,10 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { profile } from './profile'
-import { blogType } from './blogType'
-import { blockContentType } from './blockContentType'
+import { type SchemaTypeDefinition } from "sanity";
+import { profile } from "./profile";
+import { blogType } from "./blogType";
+import { blockContentType } from "../objects/blockContentType";
+import { customImage } from "./customImage";
+import { categoryType } from "../Miscellaneous/categoryType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, blogType, blockContentType],
-}
+  types: [profile, blogType, blockContentType, customImage, categoryType],
+};
